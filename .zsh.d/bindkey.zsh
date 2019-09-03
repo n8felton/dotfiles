fa999d5011d7a3c42f698df3e93dce8ee79199bf
Nate Felton <n8felton@gmail.com>
2019-09-03 12:37:13 -0400

    Updating history search bindkey

--- .zsh.d/bindkey.zsh
+++ .zsh.d/bindkey.zsh
@@ -1,5 +1,12 @@
 bindkey "${terminfo[khome]}" beginning-of-line
 bindkey "${terminfo[kend]}" end-of-line
 bindkey "${terminfo[kdch1]}" delete-char
-bindkey '^[[A' up-line-or-search
-bindkey '^[[B' down-line-or-search
+
+# Search through history for previous commands matching everything up to current cursor position.
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey "^[[A" up-line-or-beginning-search # Up
+bindkey "^[[B" down-line-or-beginning-search # Down
+
