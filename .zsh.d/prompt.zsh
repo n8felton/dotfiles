348128042bb6fc2f6fbce1a6fcef7ae8fc47a947
Nate Felton <n8felton@gmail.com>
2019-09-08 14:02:08 -0400

    Updating prompt to allow for different root prompt

--- .zsh.d/prompt.zsh
+++ .zsh.d/prompt.zsh
@@ -1,1 +1,5 @@
-PROMPT='%F{green}%n%F{yellow}@%m%f:%F{cyan}%~%f %# '
+if [ $EUID -eq 0 ]; then
+	PROMPT='%F{red}%n@%m%f:%F{cyan}%~%f %# '
+else
+	PROMPT='%F{green}%n%F{yellow}@%m%f:%F{cyan}%~%f %# '
+fi
