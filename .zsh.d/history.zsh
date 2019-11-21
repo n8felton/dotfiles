4ffa8d8fa559036e4be3ae0fe348c36b5217c3bd
n8felton <n8felton@gmail.com>
2019-11-21 08:42:38 -0500

    Adding HIST_IGNORE_SPACE option

--- .zsh.d/history.zsh
+++ .zsh.d/history.zsh
@@ -10,4 +10,4 @@ setopt hist_ignore_space      # ignore commands that start with space
 setopt hist_verify            # show command with history expansion to user before running it
 setopt inc_append_history     # add commands to HISTFILE in order of execution
 setopt share_history          # share command history data
-
+setopt HIST_IGNORE_SPACE      # remove command from history when first character is a space
