32b224d335516a32148caa522a3f927fb37fad68
Nathan Felton <n8felton@gmail.com>
2021-01-22 22:34:45 -0500

    Simplifying the PROMPT

--- .zsh.d/prompt.zsh
+++ .zsh.d/prompt.zsh
@@ -1,5 +1,2 @@
-if [ $EUID -eq 0 ]; then
-	PROMPT='%F{red}%n@%m%f:%F{cyan}%~%f %# '
-else
-	PROMPT='%F{green}%n%F{yellow}@%m%f:%F{cyan}%~%f %# '
-fi
+PROMPT='%(#:%F{red}%n@%m%f:%F{green}%n%F{yellow}@%m%f)'
+PROMPT+=':%F{cyan}%~%f %# '
