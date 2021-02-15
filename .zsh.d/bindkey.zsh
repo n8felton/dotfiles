5e9e96552bcb1c6d35c8353ce5fd5d7af433e7f1
Nathan Felton <nlfdss@rit.edu>
2021-02-15 17:13:10 -0500

    Adding some more keybinds

--- .zsh.d/bindkey.zsh
+++ .zsh.d/bindkey.zsh
@@ -2,6 +2,11 @@ bindkey "${terminfo[khome]}" beginning-of-line
 bindkey "${terminfo[kend]}" end-of-line
 bindkey "${terminfo[kdch1]}" delete-char
 
+bindkey "^[b" backward-word
+bindkey "^[f" forward-word
+bindkey "^A" beginning-of-line
+bindkey "^E" end-of-line
+
 # Search through history for previous commands matching everything up to current cursor position.
 autoload -U up-line-or-beginning-search
 autoload -U down-line-or-beginning-search
@@ -9,4 +14,3 @@ zle -N up-line-or-beginning-search
 zle -N down-line-or-beginning-search
 bindkey "^[[A" up-line-or-beginning-search # Up
 bindkey "^[[B" down-line-or-beginning-search # Down
-
