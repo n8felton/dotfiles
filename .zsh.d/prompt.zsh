286c051464b8d260cdf75f2ba15343f15174f104
n8felton <n8felton@gmail.com>
2022-12-02 19:39:40 -0500

    Change precmd to chpwd; update prompt

--- .zsh.d/prompt.zsh
+++ .zsh.d/prompt.zsh
@@ -6,7 +6,19 @@ setopt PROMPT_SUBST
 PS1='%(#:%F{red}%n@%m%f:%F{green}%n%F{yellow}@%m%f)'
 PS1+=':%F{cyan}%~%f %# '
 
-precmd() {
-	# Change the xterm title to the hsotname
-	printf "\033]0;%s\007" "${HOST%%.*}"
+chpwd() {
+	# Convert "${HOME}" to ~ for PWD
+	if [[ "${PWD}" == "${HOME}" ]]; then
+		PWD="~"
+	fi
+	# Change the xterm title to the hostname if we're in an SSH session.
+	# Include the PWD
+	if [[ -n ${SSH_CLIENT} || -n ${SSH_TTY} ]]; then
+		printf "\033]0;%s | %s\007" "${HOST%%.*}" "${PWD##*/}"
+	else
+		# This should be a localhost prompt, so exclude hostname
+		if [[ ${0} == "-zsh" ]]; then
+			printf "\033]0;%s\007" "${PWD##*/}"
+		fi
+	fi
 }
